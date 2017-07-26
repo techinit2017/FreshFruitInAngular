834c039967c140dce426af76f6f7cd46fa9a0ead
tech.init.2017 <tech.init.2017@gmail.com>
2017-07-27 04:04:43 +0530

    Css Alignment Done @Sunil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
+
+import { SearchService } from '../search/search.service';
 import {Component, ViewChild} from '@angular/core';
 import { MdMenuTrigger } from '@angular/material';
+import {SearchComponent} from '../search/search.component';
 
 
 @Component({
@@ -8,13 +11,7 @@ import { MdMenuTrigger } from '@angular/material';
   styleUrls: ['home.component.css']
 })
 export class HomeComponent {
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
+ 
 }
  export class MyComponent {
   @ViewChild(MdMenuTrigger) trigger: MdMenuTrigger;
