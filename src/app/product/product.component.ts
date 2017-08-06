a846a091706578939bc041e860a1e5fa2ab1894d
Harsh Maheswari <harsh.maheshwari001@gmail.com>
2017-08-06 12:56:32 +0530

    @Harsh Code Merged till 6-July

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,14 +9,87 @@ import {Component, OnInit} from '@angular/core';
   providers: [ProductService],
 })
 export class ProductComponent implements OnInit {
+
   prodlist = 'Product';
   iProduct: IProduct[];
-  errorMsg: string;
+  errorMsg:  string;
   postData: string;
-  constructor(private _productService: ProductService) {}
-
-  ngOnInit() {
-    this._productService.getProducts()
-      .subscribe(iProduct => this.iProduct = iProduct, err => this.errorMsg = <any>err);
+  filteredItems: IProduct[];
+  pages = 4;
+  pageSize = 5;
+  pageNumber = 0;
+  currentIndex = 1;
+  items: IProduct[];
+  pagesIndex: Array<number>;
+  pageStart = 1;
+  inputName: '';
+  constructor(private _productService: ProductService) {
+       
   }
+  
+   ngOnInit() {
+    this._productService.getProducts()
+    .subscribe(iProduct => this.iProduct = iProduct, err => this.errorMsg = <any>err);
+  }
+  
+  /*init() {
+         this.currentIndex = 1;
+         this.pageStart = 1;
+         this.pages = 4;
+
+         this.pageNumber = parseInt('' + (this.filteredItems.length / this.pageSize));
+         if (this.filteredItems.length % this.pageSize != 0) {
+            this.pageNumber ++;
+         }
+    
+         if (this.pageNumber  < this.pages) {
+               this.pages =  this.pageNumber;
+         }
+       
+         this.refreshItems();
+         console.log('this.pageNumber:' + this.pageNumber);
+   }
+  
+
+ 
+
+
+  fillArray(): any {
+    var obj = new Array();
+    for (var index = this.pageStart; index < this.pageStart + this.pages; index++) {
+      obj.push(index);
+    }
+    return obj;
+  }
+  refreshItems() {
+    this.items = this.filteredItems.slice((this.currentIndex - 1) * this.pageSize, (this.currentIndex) * this.pageSize);
+    this.pagesIndex = this.fillArray();
+  }
+  prevPage() {
+    if (this.currentIndex > 1) {
+      this.currentIndex--;
+    }
+    if (this.currentIndex < this.pageStart) {
+      this.pageStart = this.currentIndex;
+    }
+    this.refreshItems();
+  }
+  nextPage() {
+    if (this.currentIndex < this.pageNumber) {
+      this.currentIndex++;
+    }
+    if (this.currentIndex >= (this.pageStart + this.pages)) {
+      this.pageStart = this.currentIndex - this.pages + 1;
+    }
+
+    this.refreshItems();
+  }
+  setPage(index: number) {
+    this.currentIndex = index;
+    this.refreshItems();
+  }
+
+  */
+  
+  
 }
--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -2,4 +2,12 @@ export interface IProduct {
   ProductID: number;
   ProductName: string;
   url: string;
+  price: number;
+  Quantity: number;
+  Variety: string;
+  Grade: string;
+  Country:  string;
+  available: string;
+  packaging: string;
+  isActive: number;
 }
